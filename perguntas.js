419cf5c78f07f3e2aaeb2b39271a3ed7ab260087
matheusalvesdossantos <146347322+matheusalvesdossantos@users.noreply.github.com>
2024-09-20 09:28:00 -0300

    .

--- perguntas.js
+++ perguntas.js
@@ -1,23 +1,23 @@
 criaCartao(
     'Programação',
-    'O que é Python?',
+    'quem é michel diecsom?',
     'O Python é uma linguagem de programação'
 )
 
 criaCartao(
     'Geografia',
-    'Qual a capital da França?',
+    'quantos geladinhos vc agunta chupar?',
     'A capital da França é Paris'
 )
 
 criaCartao(
     'Programação',
-    'O que é uma função?',
+    'O que é uma casa de fumo?',
     'Uma função é um bloco de código que executa alguma tarefa'
 )
 
 criaCartao(
     'Lingua inglesa',
-    'Como se diz oi em Inglês?',
+    'com quantos paus se faz um morango?',
     'Oi em ingles é HI (RAI)'
 )
